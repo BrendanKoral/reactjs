657af799ca69c0d3f06028176e9f9f805608b191
Brendan Koral <brendanhkoral@gmail.com>
2016-04-25 15:43:00 -0500

    Finished bulletin board

--- js/Note.js
+++ js/Note.js
@@ -2,6 +2,19 @@ var Note = React.createClass({
     getInitialState: function () {
         return {editing: false}
     },
+    componentWillMount: function () {
+        this.style = {
+          right: this.randomBetween(0, window.innerWidth - 150) + 'px',
+            top: this.randomBetween(0, window.innerHeight - 150) + 'px',
+            transform: 'rotate(' + this.randomBetween(-15, 15) + 'deg)'
+        };
+    },
+    componentDidMount: function () {
+      $(this.getDOMNode()).draggable();
+    },
+    randomBetween: function (min, max) {
+        return (min + Math.ceil(Math.random() * max));
+    },
     edit: function(){
         this.setState({editing: true});
     },
@@ -13,7 +26,8 @@ var Note = React.createClass({
       this.props.onRemove(this.props.index);
     },
     renderDisplay: function () {
-            return (<div className="note">
+            return (<div className="note"
+                style={this.style}>
                     <p>{this.props.children}</p>
                 <span>
                     <button onClick={this.edit}
@@ -26,7 +40,8 @@ var Note = React.createClass({
     },
     renderForm: function() {
         return (
-            <div className="note">
+            <div className="note"
+                 style={this.style}>
                 <textarea ref="newText" defaultValue={this.props.children} className="form-control" />
                 <button onClick={this.save} className="btn btn-success btn-sm glyphicon glyphicon-floppy-disk" />
             </div>
@@ -57,17 +72,34 @@ var Board = React.createClass({
     },
     getInitialState: function() {
       return {
-          notes: [
-              'Call Bill',
-              'Email Lisa',
-              'Make dentist appt',
-              'Send proposal'
-          ]
+          notes: []
       };
+    },
+    nextId: function () {
+      this.uniqueId = this.uniqueId || 0;
+        return this.uniqueId++;
+    },
+    componentWillMount: function () {
+      var self = this;
+        if(this.props.count) {
+            $.getJSON("http://baconipsum.com/api/?type=all-meat&sentences=" + this.props.count + "&start-with-lorem=1&callback=?", function (results) {
+              results[0].split('. ').forEach(function(sentence) {
+                  self.add(sentence.substring(0,40));
+              });
+            });
+        }
+    },
+    add: function (text) {
+        var arr = this.state.notes;
+        arr.push({
+            id: this.nextId(),
+            note: text
+        });
+        this.setState({notes: arr})
     },
     update: function(newText, i) {
         var arr = this.state.notes;
-        arr[i] = newText;
+        arr[i].note = newText;
         this.setState({notes: arr});
     },
     remove: function(i) {
@@ -77,21 +109,22 @@ var Board = React.createClass({
     },
     eachNote: function(note, i) {
       return (
-          <Note key={i}
+          <Note key={note.id}
             index={i}
                 onChange={this.update}
                 onRemove={this.remove}
-          >{note}</Note>
+          >{note.note}</Note>
       );
     },
     render: function () {
         return (<div className="board">
                 {this.state.notes.map(this.eachNote)}
+                <button className="btn btn-sm  btn-success glyphicon glyphicon-plus" onClick={this.add.bind(null, "New Note")}/>
         </div>
         );
     }
 });
 
 
-React.render(<Board count={10}/>,
+React.render(<Board count={50}/>,
     document.getElementById('react-container'));
